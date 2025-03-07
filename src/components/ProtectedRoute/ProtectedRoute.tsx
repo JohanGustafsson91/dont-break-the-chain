523c2a90d760b5757afaaee9115b9b07e44f4bcf
Johan Gustafsson <johan.gustafsson91@gmail.com>
2025-03-07 13:29:51 +0100

    pwa

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -13,8 +13,8 @@ export const ProtectedRoute = ({
 
   if (status === "PENDING") {
     return (
-      <div className="page">
-        <p>Loading...</p>
+      <div className="page page-center">
+        <p className="loading">Loading</p>
       </div>
     );
   }
